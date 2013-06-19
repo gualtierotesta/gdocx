f1c5c4855b7eef578189792cb7eea86afa604d7e
Gualtiero Testa <gualtiero.testa@gmail.com>
2013-06-19 14:55:31 +0200

    New demo

---
+++ src/main/java/it/gualtierotesta/demo/GPDemo.java
@@ -0,0 +1,11 @@
+package it.gualtierotesta.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gualtiero
+ * Date: 19/06/13
+ * Time: 14.00
+ * To change this template use File | Settings | File Templates.
+ */
+public class GPDemo {
+}
